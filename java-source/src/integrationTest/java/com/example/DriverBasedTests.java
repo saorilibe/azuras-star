bcbc3f9d6480130a35fcb58d77dd522fe25a2857
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-06-27 13:01:31 +0000

    Added first files

--- java-source/src/integrationTest/java/com/example/DriverBasedTests.java
+++ java-source/src/integrationTest/java/com/example/DriverBasedTests.java
@@ -3,4 +3,5 @@ package com.example;
 import com.google.common.collect.ImmutableList;
 import net.corda.core.concurrent.CordaFuture;
 import net.corda.core.identity.CordaX500Name;
-import net.corda.core.utilities.NetworkHost
+import net.corda.core.utilities.NetworkHostAndPort;
+import net.corda.testing.core.TestIdenti
