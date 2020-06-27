8c0143dace0fccb194a41664bd9a770bba44293b
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-06-27 13:16:42 +0000

    bug fix...EJB

--- java-source/src/integrationTest/java/com/example/DriverBasedTests.java
+++ java-source/src/integrationTest/java/com/example/DriverBasedTests.java
@@ -4,4 +4,6 @@ import com.google.common.collect.ImmutableList;
 import net.corda.core.concurrent.CordaFuture;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.utilities.NetworkHostAndPort;
-import net.corda.testing.core.TestIdenti
+import net.corda.testing.core.TestIdentity;
+import net.corda.testing.driver.DriverParameters;
+import net.co
