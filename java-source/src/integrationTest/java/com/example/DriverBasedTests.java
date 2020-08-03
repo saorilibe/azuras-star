1909bae6a55afbc5b03525d7c8474d998de77bf7
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-08-03 12:08:58 +0000

    js cleanup

--- java-source/src/integrationTest/java/com/example/DriverBasedTests.java
+++ java-source/src/integrationTest/java/com/example/DriverBasedTests.java
@@ -42,4 +42,4 @@ public class DriverBasedTests {
                 // started and can communicate. This is a very basic test, in practice tests would be starting flows,
                 // and verifying the states in the vault and other important metrics to ensure that your CorDapp is working
                 // as intended.
-                assert
+                assertEquals(partyAHandle.getRpc().wellKnownPartyFromX500Name(bankB.g
