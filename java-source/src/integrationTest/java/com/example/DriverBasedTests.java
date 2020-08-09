e587307dca20ed9a46c897fb91ac0cf200ba1df2
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-08-09 12:34:58 +0000

    Update lab2.md

--- java-source/src/integrationTest/java/com/example/DriverBasedTests.java
+++ java-source/src/integrationTest/java/com/example/DriverBasedTests.java
@@ -57,4 +57,4 @@ public class DriverBasedTests {
         driver(new DriverParameters().withIsDebug(true).withStartNodesInProcess(true), dsl -> {
 
             List<CordaFuture<NodeHandle>> handleFutures = ImmutableList.of(
-       
+                    dsl.startNode(new NodeParameters().withProvidedNam
