60982b01868a80b88930e75ba94a91a740f5422d
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-08-20 20:34:02 +0000

    Tweak formatting

--- java-source/src/main/java/com/example/api/Api.java
+++ java-source/src/main/java/com/example/api/Api.java
@@ -24,3 +24,84 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
+import static java.util.stream.Collectors.toList;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.CREATED;
+import static javax.ws.rs.core.Response.Status.OK;
+
+// This API is accessible from /api/example. All paths specified below are relative to it.
+@Path("resources")
+public class Api {
+    private final CordaRPCOps rpcOps;
+    private final CordaX500Name myLegalName;
+
+    private final List<String> serviceNames = ImmutableList.of("Notary");
+
+    static private final Logger logger = LoggerFactory.getLogger(Api.class);
+
+    public Api(CordaRPCOps rpcOps) {
+        this.rpcOps = rpcOps;
+        this.myLegalName = rpcOps.nodeInfo().getLegalIdentities().get(0).getName();
+    }
+
+    /**
+     * Returns the node's name.
+     */
+    @GET
+    @Path("me")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, CordaX500Name> whoami() {
+        return ImmutableMap.of("me", myLegalName);
+    }
+
+    /**
+     * Returns all parties registered with the [NetworkMapService]. These names can be used to look up identities
+     * using the [IdentityService].
+     */
+    @GET
+    @Path("peers")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, List<CordaX500Name>> getPeers() {
+        List<NodeInfo> nodeInfoSnapshot = rpcOps.networkMapSnapshot();
+        return ImmutableMap.of("peers", nodeInfoSnapshot
+                .stream()
+                .map(node -> node.getLegalIdentities().get(0).getName())
+                .filter(name -> !name.equals(myLegalName) && !serviceNames.contains(name.getOrganisation()))
+                .collect(toList()));
+    }
+
+    /**
+     * Displays all IOU states that exist in the node's vault.
+     */
+    @GET
+    @Path("ious")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<StateAndRef<IOUState>> getIOUs() {
+        return rpcOps.vaultQuery(IOUState.class).getStates();
+    }
+
+    /**
+     * Initiates a flow to agree an IOU between two parties.
+     *
+     * Once the flow finishes it will have written the IOU to ledger. Both the hospital and the patient will be able to
+     * see it when calling /api/example/ious on their respective nodes.
+     *
+     * This end-point takes a Party name parameter as part of the path. If the serving node can't find the other party
+     * in its network map cache, it will return an HTTP bad request.
+     *
+     * The flow is invoked asynchronously. It returns a future when the flow's call() method returns.
+     */
+    @PUT
+    @Path("create-iou")
+    public Response createIOU(@QueryParam("iouName") String iouName, @QueryParam("iouAge") int iouAge,
+                              @QueryParam("iouGender") String iouGender, @QueryParam("iouHeight") int iouHeight,
+                              @QueryParam("iouWeight") int iouWeight, @QueryParam("iouBloodGroup") String iouBloodGroup,
+                              @QueryParam("iouDiagnosis") String iouDiagnosis, @QueryParam("iouMedicine") String iouMedicine,
+                              @QueryParam("partyName") CordaX500Name partyName) throws InterruptedException, ExecutionException {
+
+        if (iouName == null) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'iouName' missing or has wrong format.\n").build();
+        }
+        if (iouAge <= 0) {
+            return Response.status(BAD_REQUEST).entity("Query parameter 'iouAge' is negative.\n").build();
