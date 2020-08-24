6eae3c72d8b631146510ce631ee68f9eebc2e1fc
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-08-24 20:08:15 +0000

    remove obj folders

--- java-source/src/main/java/com/example/flow/IssueFlow.java
+++ java-source/src/main/java/com/example/flow/IssueFlow.java
@@ -4,4 +4,5 @@ import co.paralleluniverse.fibers.Suspendable;
 import com.example.contract.IssueContract;
 import com.example.state.IOUState;
 import com.google.common.collect.ImmutableList;
-import com
+import com.google.common.collect.ImmutableSet;
+import net.corda.core.co
