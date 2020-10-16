f18de5f72d372b60e223851da203173b9eac7d1d
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-10-16 13:41:59 +0000

    Lead développeur python / django

--- java-source/src/main/java/com/example/flow/IssueFlow.java
+++ java-source/src/main/java/com/example/flow/IssueFlow.java
@@ -86,4 +86,5 @@ public class IssueFlow {
          */
         @Suspendable
         @Override
-        public SignedTransaction
+        public SignedTransaction call() throws FlowException {
+            // Obtain a refe
