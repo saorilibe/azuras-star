8246c54f4a19d753a820cb75cb117b01ee16d969
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-10-23 21:14:22 +0000

    Add some more tests.

--- java-source/src/main/java/com/example/flow/IssueFlow.java
+++ java-source/src/main/java/com/example/flow/IssueFlow.java
@@ -94,4 +94,4 @@ public class IssueFlow {
             progressTracker.setCurrentStep(GENERATING_TRANSACTION);
             // Generate an unsigned transaction.
             Party me = getOurIdentity();
-            IOUState iouState = new IOUState(me, otherParty, iouName, iouAge, iouGender, iouHeight, iouWeight
+            IOUState iouState = new IOUState(me, otherParty, iouName, iouAge, iouGender, iouHeight, iouWeight, iouBloodGroup, iouDiagnosis, iouMedicine, new U
