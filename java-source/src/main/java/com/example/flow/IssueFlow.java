e3b089e7c4202ef62da215d82d3698f658f188de
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-12-11 05:31:40 +0000

    condition

--- java-source/src/main/java/com/example/flow/IssueFlow.java
+++ java-source/src/main/java/com/example/flow/IssueFlow.java
@@ -149,4 +149,5 @@ public class IssueFlow {
                         ContractState output = stx.getTx().getOutputs().get(0).getData();
                         require.using("This must be an IOU transaction.", output instanceof IOUState);
                         IOUState iou = (IOUState) output;
-                            require.using("I won't accept patients with a name that is missing or has wrong 
+                            require.using("I won't accept patients with a name that is missing or has wrong format.", iou.getName() != null);
+                      
