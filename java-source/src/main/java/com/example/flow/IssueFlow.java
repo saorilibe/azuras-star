9e0f946a2e25cf51cdcd8a803800f300cc2ca21c
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-12-20 11:33:23 +0000

    1.20

--- java-source/src/main/java/com/example/flow/IssueFlow.java
+++ java-source/src/main/java/com/example/flow/IssueFlow.java
@@ -153,4 +153,4 @@ public class IssueFlow {
                             require.using("I won't accept patients with age that is negtive.", iou.getAge() <= 150);
                             require.using("I won't accept patients with a wrong gender information.", iou.getGender().equals("Male") || iou.getGender().equals("Female") || iou.getGender().equals("Other"));
                             require.using("I won't accept patients with a wrong blood group information.", iou.getBloodGroup().equals("A(positive)") || iou.getBloodGroup().equals("A(negative)")
-                                || 
+                                || iou.getBloodGroup().equals("B(positive)") || io
