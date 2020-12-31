8e0f1f0fb8abb957673172da27a7145a08f47c57
saorilibe <116712846+saorilibe@users.noreply.github.com>
2020-12-31 15:07:39 +0000

    Added some more unit tests

--- java-source/src/main/java/com/example/schema/IOUSchemaV1.java
+++ java-source/src/main/java/com/example/schema/IOUSchemaV1.java
@@ -1,4 +1,5 @@
 package com.example.schema;
 
 import com.google.common.collect.ImmutableList;
-import net.corda.core.
+import net.corda.core.schemas.MappedSchema;
+import net.corda.core.schemas.Pers
