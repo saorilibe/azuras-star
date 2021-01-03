667844e8e14821420d9a3723571ffe292322c6f8
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-01-03 06:17:35 +0000

    Add codebeat badge

--- java-source/src/main/java/com/example/schema/IOUSchemaV1.java
+++ java-source/src/main/java/com/example/schema/IOUSchemaV1.java
@@ -2,4 +2,6 @@ package com.example.schema;
 
 import com.google.common.collect.ImmutableList;
 import net.corda.core.schemas.MappedSchema;
-import net.corda.core.schemas.Pers
+import net.corda.core.schemas.PersistentState;
+
+import javax.persistence.Co
