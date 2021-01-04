2fe73e2420912e07fcd4f9feb036168824594fe9
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-01-04 10:39:24 +0000

    remove bad import

--- java-source/src/main/java/com/example/schema/IOUSchemaV1.java
+++ java-source/src/main/java/com/example/schema/IOUSchemaV1.java
@@ -4,4 +4,6 @@ import com.google.common.collect.ImmutableList;
 import net.corda.core.schemas.MappedSchema;
 import net.corda.core.schemas.PersistentState;
 
-import javax.persistence.Co
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persis
