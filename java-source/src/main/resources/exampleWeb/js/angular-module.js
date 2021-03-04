ee9134f07d7bc84f3be503dfd57ac58b97fed3ad
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-03-04 17:58:14 +0000

    some more tests

---
+++ java-source/src/main/resources/exampleWeb/js/angular-module.js
@@ -0,0 +1,51 @@
+
+"use strict";
+
+// --------
+// WARNING:
+// --------
+
+// THIS CODE IS ONLY MADE AVAILABLE FOR DEMONSTRATION PURPOSES AND IS NOT SECURE!
+// DO NOT USE IN PRODUCTION!
+
+// FOR SECURITY REASONS, USING A JAVASCRIPT WEB APP HOSTED VIA THE CORDA NODE IS
+// NOT THE RECOMMENDED WAY TO INTERFACE WITH CORDA NODES! HOWEVER, FOR THIS
+// PRE-ALPHA RELEASE IT'S A USEFUL WAY TO EXPERIMENT WITH THE PLATFORM AS IT ALLOWS
+// YOU TO QUICKLY BUILD A UI FOR DEMONSTRATION PURPOSES.
+
+// GOING FORWARD WE RECOMMEND IMPLEMENTING A STANDALONE WEB SERVER THAT AUTHORISES
+// VIA THE NODE'S RPC INTERFACE. IN THE COMING WEEKS WE'LL WRITE A TUTORIAL ON
+// HOW BEST TO DO THIS.
+
+const app = angular.module('demoAppModule', ['ui.bootstrap']);
+
+// Fix for unhandled rejections bug.
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+app.controller('DemoAppController', function($http, $location, $uibModal) {
+    const demoApp = this;
+
+    // We identify the node.
+    const apiBaseURL = "/api/resources/";
+    let peers = [];
+
+    $http.get(apiBaseURL + "me").then((response) => demoApp.thisNode = response.data.me);
+
+    $http.get(apiBaseURL + "peers").then((response) => peers = response.data.peers);
+
+    demoApp.openModal = () => {
+        const modalInstance = $uibModal.open({
+            templateUrl: 'demoAppModal.html',
+            controller: 'ModalInstanceCtrl',
+            controllerAs: 'modalInstance',
+            resolve: {
+                demoApp: () => demoApp,
+                apiBaseURL: () => apiBaseURL,
+                peers: () => peers
+            }
+        });
+
+        modalInstance.result.then(() => {}, () => {});
+    };
