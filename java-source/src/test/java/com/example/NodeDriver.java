c5c623736669f539e9ef5be8cc5de87fd069366b
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-03-06 19:45:58 +0000

    Added missing dependency.

--- java-source/src/test/java/com/example/NodeDriver.java
+++ java-source/src/test/java/com/example/NodeDriver.java
@@ -2,4 +2,6 @@ package com.example;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableS
+import com.google.common.collect.ImmutableSet;
+import net.corda.core.concurrent.CordaFuture;
+import ne
