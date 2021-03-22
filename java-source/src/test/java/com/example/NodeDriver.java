f209bfb370827af0f40df85ff913e0646a06807b
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-03-22 06:56:42 +0000

    some adjustments

--- java-source/src/test/java/com/example/NodeDriver.java
+++ java-source/src/test/java/com/example/NodeDriver.java
@@ -20,4 +20,5 @@ import static net.corda.testing.driver.Driver.driver;
  */
 public class NodeDriver {
     public static void main(String[] args) {
-        final User user = new User("user1", "test", ImmutableSet.o
+        final User user = new User("user1", "test", ImmutableSet.of("ALL"));
+        driver(new DriverParameters().withW
