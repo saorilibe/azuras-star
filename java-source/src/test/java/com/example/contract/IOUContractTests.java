4aa885e6f412176c4d5078d8112c1a161848a002
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-04-16 20:03:59 +0000

    Fixed project file

--- java-source/src/test/java/com/example/contract/IOUContractTests.java
+++ java-source/src/test/java/com/example/contract/IOUContractTests.java
@@ -18,3 +18,71 @@ public class IOUContractTests {
     static private TestIdentity miniCorp = new TestIdentity(new CordaX500Name("MiniCorp", "London", "GB"));
 //    static private int iouValue = 1;
     static private String iouName = "F";
+
+    @Test
+    public void transactionMustIncludeCreateCommand() {
+        ledger(ledgerServices, (ledger -> {
+            ledger.transaction(tx -> {
+                tx.output(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.fails();
+                tx.command(ImmutableList.of(megaCorp.getPublicKey(), miniCorp.getPublicKey()), new IssueContract.Commands.Create());
+                tx.verifies();
+                return null;
+            });
+            return null;
+        }));
+    }
+
+    @Test
+    public void transactionMustHaveNoInputs() {
+        ledger(ledgerServices, (ledger -> {
+            ledger.transaction(tx -> {
+                tx.input(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.output(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.command(ImmutableList.of(megaCorp.getPublicKey(), miniCorp.getPublicKey()), new IssueContract.Commands.Create());
+                tx.failsWith("No inputs should be consumed when issuing an IOU.");
+                return null;
+            });
+            return null;
+        }));
+    }
+
+    @Test
+    public void transactionMustHaveOneOutput() {
+        ledger(ledgerServices, (ledger -> {
+            ledger.transaction(tx -> {
+                tx.output(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.output(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.command(ImmutableList.of(megaCorp.getPublicKey(), miniCorp.getPublicKey()), new IssueContract.Commands.Create());
+                tx.failsWith("Only one output state should be created.");
+                return null;
+            });
+            return null;
+        }));
+    }
+
+    @Test
+    public void hospitalMustSignTransaction() {
+        ledger(ledgerServices, (ledger -> {
+            ledger.transaction(tx -> {
+                tx.output(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.command(miniCorp.getPublicKey(), new IssueContract.Commands.Create());
+                tx.failsWith("All of the participants must be signers.");
+                return null;
+            });
+            return null;
+        }));
+    }
+
+    @Test
+    public void patientMustSignTransaction() {
+        ledger(ledgerServices, (ledger -> {
+            ledger.transaction(tx -> {
+                tx.output(IOU_CONTRACT_ID, new IOUState(iouName, miniCorp.getParty(), megaCorp.getParty(), new UniqueIdentifier()));
+                tx.command(megaCorp.getPublicKey(), new IssueContract.Commands.Create());
+                tx.failsWith("All of the participants must be signers.");
+                return null;
+            });
+            return null;
+        }));
+    }
