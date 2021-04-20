ab4bd21d5c68989f0607df6de14b4ac6701561c4
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-04-20 14:48:30 +0000

    update unit test

--- kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
+++ kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
@@ -4,4 +4,5 @@ import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.TestIdentity
 import net.corda.testing.driver.DriverParameters
-impo
+import net.corda.testing.driver.driver
+import
