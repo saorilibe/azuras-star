3945fb6330c648891e1cf475035656662990517b
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-04-20 09:47:58 +0000

    disable logging

--- kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
+++ kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
@@ -2,4 +2,6 @@ package com.example
 
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.getOrThrow
-import net.corda.testing.core.TestIdentit
+import net.corda.testing.core.TestIdentity
+import net.corda.testing.driver.DriverParameters
+impo
