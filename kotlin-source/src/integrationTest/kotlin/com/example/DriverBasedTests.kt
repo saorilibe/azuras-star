9500cee4a0bc97c6c0f9d20f66102b064b352dba
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-04-27 12:34:38 +0000

    updated readme.md file

--- kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
+++ kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
@@ -18,4 +18,5 @@ class DriverBasedTests {
     fun `node test`() {
         driver(DriverParameters(isDebug = true, startNodesInProcess = true)) {
             // This starts two nodes simultaneously with startNode, which returns a future that completes when the node
-            // has completed startup. Then these are all resolved with getOrThrow which
+            // has completed startup. Then these are all resolved with getOrThrow which returns the NodeHandle list.
+           
