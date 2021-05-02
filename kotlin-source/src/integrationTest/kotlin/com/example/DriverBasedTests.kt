1b0ec6c2f65d64bd28b2d6831437e3dc7a737d67
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-05-02 17:29:30 +0000

    [fixed] stability issue

--- kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
+++ kotlin-source/src/integrationTest/kotlin/com/example/DriverBasedTests.kt
@@ -27,4 +27,5 @@ class DriverBasedTests {
             // This test makes an RPC call to retrieve another node's name from the network map, to verify that the
             // nodes have started and can communicate. This is a very basic test, in practice tests would be starting
             // flows, and verifying the states in the vault and other important metrics to ensure that your CorDapp is
-       
+            // working as intended.
+            assertEquals
