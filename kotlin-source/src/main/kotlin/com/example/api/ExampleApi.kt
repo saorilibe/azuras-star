6adc3d2358936b92c8ca2695eab4f27cf4152ec0
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-07-08 05:33:58 +0000

    Grammar correction

--- kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
+++ kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
@@ -2,4 +2,5 @@ package com.example.api
 
 import com.example.flow.ExampleFlow.Initiator
 import com.example.schema.IOUSchemaV1
-import com.ex
+import com.example.state.IOUState
+import net.corda.core.identity.Cor
