beb2c584ad36b0bfcb5d3f028dd1748c1f7881d8
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-07-09 07:06:02 +0000

    new line at end of file

--- kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
+++ kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
@@ -3,4 +3,5 @@ package com.example.api
 import com.example.flow.ExampleFlow.Initiator
 import com.example.schema.IOUSchemaV1
 import com.example.state.IOUState
-import net.corda.core.identity.Cor
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.messaging.CordaRPCOp
