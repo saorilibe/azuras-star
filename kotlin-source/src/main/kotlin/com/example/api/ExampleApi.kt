8b2e3cbe19776b136ccd15daba041296e23adb8f
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-07-25 06:52:34 +0000

    Spec fixes

--- kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
+++ kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
@@ -9,4 +9,5 @@ import net.corda.core.messaging.startTrackedFlow
 import net.corda.core.messaging.vaultQueryBy
 import net.corda.core.node.services.IdentityService
 import net.corda.core.node.services.Vault
-import net.corda.core.node.services.
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.node.services
