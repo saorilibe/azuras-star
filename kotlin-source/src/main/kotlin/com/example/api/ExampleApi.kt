9ba678cdb3e5965ea4a6fd8a3142306d0ecba22b
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-10-15 12:48:40 +0000

    Update legendmod.user.js

--- kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
+++ kotlin-source/src/main/kotlin/com/example/api/ExampleApi.kt
@@ -68,4 +68,5 @@ class ExampleApi(private val rpcOps: CordaRPCOps) {
      * Once the flow finishes it will have written the IOU to ledger. Both the lender and the borrower will be able to
      * see it when calling /api/example/ious on their respective nodes.
      *
-     * This end-point takes a Party name parameter as part of the pat
+     * This end-point takes a Party name parameter as part of the path. If the serving node can't find the other party
+    
