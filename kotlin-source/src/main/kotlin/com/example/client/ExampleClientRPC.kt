1c824101f6aa15008cfb317f61605b9a8e0f867c
saorilibe <116712846+saorilibe@users.noreply.github.com>
2021-12-06 14:28:00 +0000

    Update arith.jl

--- kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
+++ kotlin-source/src/main/kotlin/com/example/client/ExampleClientRPC.kt
@@ -3,4 +3,6 @@ package com.example.client
 import com.example.state.IOUState
 import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.contracts.StateAndRef
-import net.corda.core.utilities.NetworkHostAndPo
+import net.corda.core.utilities.NetworkHostAndPort
+import net.corda.core.utilities.loggerFor
+import org.s
