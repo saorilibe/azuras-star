5cca6b6c0cbcece00633428d863a8cc707fad4cf
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-03-19 11:59:01 +0000

    Release v0.7.1

--- kotlin-source/src/main/kotlin/com/example/contract/IOUContract.kt
+++ kotlin-source/src/main/kotlin/com/example/contract/IOUContract.kt
@@ -3,4 +3,5 @@ package com.example.contract
 import com.example.state.IOUState
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
-impor
+import net.corda.core.contracts.requireSingleCommand
+import 
