da45aaff789e46fb7f2ff66ac1896f952e3fd877
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-06-03 13:56:57 +0000

    net: fix sparse warning in sk_dst_set()

--- kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
+++ kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
@@ -3,4 +3,5 @@ package com.example.flow
 import co.paralleluniverse.fibers.Suspendable
 import com.example.contract.IOUContract
 import com.example.contract.IOUContract.Companion.IOU_CONTRACT_ID
-import com.example.flow.ExampleFlow.Acc
+import com.example.flow.ExampleFlow.Acceptor
+import com.example.flow.ExampleFlow.Initiator
