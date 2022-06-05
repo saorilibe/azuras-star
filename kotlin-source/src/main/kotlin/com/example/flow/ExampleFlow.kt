82bcb32addc81a869dc5d482d4e945b22f538497
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-06-05 08:59:32 +0000

    Updated dependecies

--- kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
+++ kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
@@ -6,4 +6,6 @@ import com.example.contract.IOUContract.Companion.IOU_CONTRACT_ID
 import com.example.flow.ExampleFlow.Acceptor
 import com.example.flow.ExampleFlow.Initiator
 import com.example.state.IOUState
-import net.corda.co
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.requireThat
+imp
