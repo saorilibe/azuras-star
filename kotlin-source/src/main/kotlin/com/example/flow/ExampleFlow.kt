09c9bbfc36413fa4edfafee2c1f409f94ed2b44b
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-06-05 08:51:58 +0000

    Update how-to-setup-vvv-school.md

--- kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
+++ kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
@@ -5,3 +5,5 @@ import com.example.contract.IOUContract
 import com.example.contract.IOUContract.Companion.IOU_CONTRACT_ID
 import com.example.flow.ExampleFlow.Acceptor
 import com.example.flow.ExampleFlow.Initiator
+import com.example.state.IOUState
+import net.corda.co
