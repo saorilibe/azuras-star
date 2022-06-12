8379b34412a60a10744e9b57f2746f6bc4304b2f
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-06-12 21:08:41 +0000

    continue refactoring

--- kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
+++ kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
@@ -13,4 +13,7 @@ import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
-import net.corda.core.
+import net.corda.core.utilities.ProgressTracker.Step
+
+/**
+ * This flow a
