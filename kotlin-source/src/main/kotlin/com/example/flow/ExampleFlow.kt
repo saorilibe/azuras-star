ef250e632a1675154794eca81cf39eadcdb6cb35
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-06-12 18:54:39 +0000

    100% test coverage.

--- kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
+++ kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
@@ -10,4 +10,6 @@ import net.corda.core.contracts.Command
 import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
-import net.corda.core.transactions.SignedTransac
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import n
