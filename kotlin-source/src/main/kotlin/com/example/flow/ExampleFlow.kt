f66b6d8a6cca20029e7ab966f4607532a33d297e
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-11-02 22:26:46 +0000

    Update 8

--- kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
+++ kotlin-source/src/main/kotlin/com/example/flow/ExampleFlow.kt
@@ -86,4 +86,4 @@ object ExampleFlow {
             val partSignedTx = serviceHub.signInitialTransaction(txBuilder)
 
             // Stage 4.
-  
+            progressTracker.currentStep = GATHERING_SI
