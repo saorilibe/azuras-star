76e07d7f7f063b0039e39bc3ee12dc8198519288
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-11-18 18:40:24 +0000

    1.2.3

--- kotlin-source/src/main/kotlin/com/example/plugin/ExamplePlugin.kt
+++ kotlin-source/src/main/kotlin/com/example/plugin/ExamplePlugin.kt
@@ -2,4 +2,5 @@ package com.example.plugin
 
 import com.example.api.ExampleApi
 import net.corda.core.messaging.CordaRPCOps
-import net.corda.webserv
+import net.corda.webserver.services.WebServerPluginRegistry
+import java.util.function.Fun
