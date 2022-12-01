0a1e4b2231586f8f1f901d9c412adcd072ce8fdb
saorilibe <116712846+saorilibe@users.noreply.github.com>
2022-12-01 18:44:50 +0000

    USB: ftdi_sio: correct ST Micro Connect Lite PIDs

--- kotlin-source/src/main/kotlin/com/example/schema/IOUSchema.kt
+++ kotlin-source/src/main/kotlin/com/example/schema/IOUSchema.kt
@@ -4,3 +4,5 @@ import net.corda.core.schemas.MappedSchema
 import net.corda.core.schemas.PersistentState
 import java.util.*
 import javax.persistence.Column
+import javax.persistence.Entity
+import j
