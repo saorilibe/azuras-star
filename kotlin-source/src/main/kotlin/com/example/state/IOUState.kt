a96b01f29ebb4ed195a20c8316398c063a084171
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-01-01 11:36:22 +0000

    final push

--- kotlin-source/src/main/kotlin/com/example/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/com/example/state/IOUState.kt
@@ -4,4 +4,6 @@ import com.example.schema.IOUSchemaV1
 import net.corda.core.contracts.ContractState
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
-import net.corda.core.identity.A
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+impor
