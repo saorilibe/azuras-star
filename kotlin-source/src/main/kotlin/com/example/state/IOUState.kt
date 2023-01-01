694864ac4a3f096da9631e1f45130de31d7ebc04
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-01-01 10:13:30 +0000

    remove libs

--- kotlin-source/src/main/kotlin/com/example/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/com/example/state/IOUState.kt
@@ -3,4 +3,5 @@ package com.example.state
 import com.example.schema.IOUSchemaV1
 import net.corda.core.contracts.ContractState
 import net.corda.core.contracts.LinearState
-import net.corda.core.cont
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.A
