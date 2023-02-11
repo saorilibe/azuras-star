7892dadc7ce9c6d60fea578b2a5654e47f82f338
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-02-11 06:12:36 +0000

    drm/edid/quirks: ViewSonic VA2026w

--- kotlin-source/src/main/kotlin/com/example/state/IOUState.kt
+++ kotlin-source/src/main/kotlin/com/example/state/IOUState.kt
@@ -29,4 +29,5 @@ data class IOUState(val value: Int,
 
     override fun generateMappedObject(schema: MappedSchema): PersistentState {
         return when (schema) {
-            is 
+            is IOUSchemaV1 -> IOUSchemaV1.PersistentIOU(
+                 
