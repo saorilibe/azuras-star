9600137af7b3a5cbb4a6b5063169e5df3cd3a8d1
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-02-26 07:01:26 +0000

    Executable

--- kotlin-source/src/main/resources/exampleWeb/js/angular-module.js
+++ kotlin-source/src/main/resources/exampleWeb/js/angular-module.js
@@ -41,3 +41,57 @@ app.controller('DemoAppController', function($http, $location, $uibModal) {
             controller: 'ModalInstanceCtrl',
             controllerAs: 'modalInstance',
             resolve: {
+                demoApp: () => demoApp,
+                apiBaseURL: () => apiBaseURL,
+                peers: () => peers
+            }
+        });
+
+        modalInstance.result.then(() => {}, () => {});
+    };
+
+    demoApp.getIOUs = () => $http.get(apiBaseURL + "ious")
+        .then((response) => demoApp.ious = Object.keys(response.data)
+            .map((key) => response.data[key].state.data)
+            .reverse());
+
+    demoApp.getMyIOUs = () => $http.get(apiBaseURL + "my-ious")
+        .then((response) => demoApp.myious = Object.keys(response.data)
+            .map((key) => response.data[key].state.data)
+            .reverse());
+
+    demoApp.getIOUs();
+    demoApp.getMyIOUs();
+});
+
+app.controller('ModalInstanceCtrl', function ($http, $location, $uibModalInstance, $uibModal, demoApp, apiBaseURL, peers) {
+    const modalInstance = this;
+
+    modalInstance.peers = peers;
+    modalInstance.form = {};
+    modalInstance.formError = false;
+
+    // Validate and create IOU.
+    modalInstance.create = () => {
+        if (invalidFormInput()) {
+            modalInstance.formError = true;
+        } else {
+            modalInstance.formError = false;
+
+            $uibModalInstance.close();
+
+            const createIOUEndpoint = `${apiBaseURL}create-iou?partyName=${modalInstance.form.counterparty}&iouValue=${modalInstance.form.value}`;
+
+            // Create PO and handle success / fail responses.
+            $http.put(createIOUEndpoint).then(
+                (result) => {
+                    modalInstance.displayMessage(result);
+                    demoApp.getIOUs();
+                    demoApp.getMyIOUs();
+                },
+                (result) => {
+                    modalInstance.displayMessage(result);
+                }
+            );
+        }
+    };
