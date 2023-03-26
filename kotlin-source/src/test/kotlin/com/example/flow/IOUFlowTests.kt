281f7810da3f997ece6c0419d45ccc54d6ff4158
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-03-26 16:20:54 +0000

    --ammend

--- kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
+++ kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
@@ -4,4 +4,5 @@ import com.example.state.IOUState
 import net.corda.core.contracts.TransactionVerificationException
 import net.corda.core.node.services.queryBy
 import net.corda.core.utilities.getOrThrow
-i
+import net.corda.testing.core.singleIdentity
+impor
