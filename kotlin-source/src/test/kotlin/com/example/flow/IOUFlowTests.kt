39c334ab8bb05d85332c039f1970f4cfe933445d
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-03-26 13:05:25 +0000

    owo

--- kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
+++ kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
@@ -1,4 +1,5 @@
 package com.example.flow
 
 import com.example.state.IOUState
-import net.corda.core.contracts.TransactionVeri
+import net.corda.core.contracts.TransactionVerificationException
+import net.corda.core.node.services.q
