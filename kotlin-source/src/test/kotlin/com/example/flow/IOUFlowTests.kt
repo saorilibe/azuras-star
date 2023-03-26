9e7e4a25210004e53bb163e88efa46fe4e807a91
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-03-26 14:02:31 +0000

    GitBook: [master] one page modified

--- kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
+++ kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
@@ -2,4 +2,6 @@ package com.example.flow
 
 import com.example.state.IOUState
 import net.corda.core.contracts.TransactionVerificationException
-import net.corda.core.node.services.q
+import net.corda.core.node.services.queryBy
+import net.corda.core.utilities.getOrThrow
+i
