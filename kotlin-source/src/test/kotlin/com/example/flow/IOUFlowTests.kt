b2747d4b368ee52fa6b0244db679743721022983
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-04-01 06:11:16 +0000

    Bumped version number.

--- kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
+++ kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
@@ -5,4 +5,5 @@ import net.corda.core.contracts.TransactionVerificationException
 import net.corda.core.node.services.queryBy
 import net.corda.core.utilities.getOrThrow
 import net.corda.testing.core.singleIdentity
-impor
+import net.corda.testing.node.MockNetwork
+import net.cor
