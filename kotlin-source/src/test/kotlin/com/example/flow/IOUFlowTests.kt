c93437e08fa653a6868e66b8385dab9ccce601d8
saorilibe <116712846+saorilibe@users.noreply.github.com>
2023-04-23 06:25:51 +0000

    AUTO-COMMIT templates/triple-notes.js

--- kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
+++ kotlin-source/src/test/kotlin/com/example/flow/IOUFlowTests.kt
@@ -45,4 +45,5 @@ class IOUFlowTests {
 
     @Test
     fun `SignedTransaction returned by the flow is signed by the initiator`() {
-        val flow = ExampleFlow.Initiator(1, 
+        val flow = ExampleFlow.Initiator(1, b.info.singleIdentity())
+        val future = a.startF
